6164e51adb96016ab56c7ae40045aa1fb616ce2d
Raúl Dueñas <raulduenas29@gmail.com>
2021-09-01 13:36:06 -0500

    Commit inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthModule } from './auth/auth.module';
+import { ValidarTokenGuard } from './guards/validar-token.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./protected/protected.module').then(m => m.ProtectedModule),
+    canActivate: [ ValidarTokenGuard ],
+    canLoad: [ ValidarTokenGuard ]
+  },
+  {
+    path: '**',
+    redirectTo: 'auth'
+  }
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/auth/services/auth.service.ts
@@ -0,0 +1,86 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { environment } from '../../../environments/environment';
+import { AuthResponse, Usuario } from '../interfaces/auth-interfaces';
+import { catchError, map, tap } from 'rxjs/operators';
+import { of, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private baseUrl = environment.baseUrl;
+  private _usuario!: Usuario;  
+
+  get usuario() {
+    return {...this._usuario};
+  }
+
+  constructor( private http: HttpClient) { }
+
+  registrar( name: string, password: string, email: string) {
+    const url = `${this.baseUrl}/auth/new`;
+    const body = { name, email, password };
+    
+    return this.http.post<AuthResponse>(url,body)
+      .pipe(
+        tap( ({ ok, token}) => { 
+          if ( ok ) {
+            localStorage.setItem('token', token!);
+          }
+        }),
+        map( resp=> resp.ok ),
+        catchError( err => of(err.error.msg) )
+      );
+  }
+
+  login( email: string, password: string ) {
+
+    const url = `${this.baseUrl}/auth`;
+    const body = { email, password };
+    
+    return this.http.post<AuthResponse>(url, body)
+      .pipe(
+        tap( ({ ok, token}) => { 
+          if ( ok ) {
+            localStorage.setItem('token', token!);
+          }
+        }),
+          map( resp=> resp.ok ),
+        catchError( err => of(err.error.msg) )
+      );
+  }
+
+  validarToken(): Observable<boolean> {
+  
+    const url = `${this.baseUrl}/auth/renew`;
+    const headers = new HttpHeaders()
+      .set('x-token', localStorage.getItem('token') || '');
+
+    return this.http.get<AuthResponse>( url, { headers } )
+      .pipe(
+        tap(resp => { 
+          if ( resp.ok ) {
+            localStorage.setItem('token', resp.token!);
+            this._usuario = {
+              name: resp.name!,
+              uid: resp.uid!,
+              email: resp.email!
+            };
+          }
+        }),
+        map(resp => {
+          return resp.ok
+        }),
+        catchError (err => of(false))
+      );
+  }
+
+  logout() {
+    localStorage.clear();
+  }
+
+}
+
+
